429820a963b922e6e475a7c43b97bb968ee0f72f
Filip Marc <Filipmarc71@gmail.com>
2019-10-03 15:26:04 +0200

    Adding exercise 2.

---
+++ src/Uzupełnianka/Obiekty.java
@@ -0,0 +1,26 @@
+package Uzupełnianka;
+
+public class Obiekty {
+    public static void main(String[] args) {
+        System.out.println("Klasa to plan tworzenia obiektów");
+
+        System.out.println("Obiekt to egzemplarz rzecywisty stworzony z klasy, " +
+                "posiadający dane cechy");
+
+        System.out.println("Zmienna to pudełko do którego możemy przypisać rózne wartości, " +
+                "nie tylko raz");
+
+        System.out.println("Każda zmienna musi mieć określony typ, np. int albo String");
+
+        System.out.println("W klasie mogą się znajdować zmienne, metody i konstruktory " +
+                "które są wywoływane przez operator new i służą do tworzenia nowych obiektów.");
+
+        System.out.println("Jeżeli w klasie zdefiniowano chociaż jeden konstruktor" +
+                "to kompilator nie wygeneruje dla nas konstruktora domyślnego.");
+
+        System.out.println("Zakładając, że istnieje klasa Car, to przy zapisie...");
+        System.out.println("car 1 i car 2 to dwie referencje wskazujące na ten sam obiekt");
+
+
+    }
+}
