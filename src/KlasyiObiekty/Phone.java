f00b8fdc9d86460920220b6eaf0aa46db3a554bd
Filip Marc <Filipmarc71@gmail.com>
2019-10-03 15:00:50 +0200

    Adding constructor to class one.

--- src/KlasyiObiekty/Phone.java
+++ src/KlasyiObiekty/Phone.java
@@ -1,19 +1,27 @@
 package KlasyiObiekty;
 
 public class Phone {
-    String color;
-    String size;
     String brand;
     String model;
+    String size;
+    String color;
     boolean wireless;
     boolean internet;
     boolean camera;
 
+    public Phone(String bra, String mod, String siz, String colo, boolean wireles, boolean interne, boolean camer) {
+        brand = bra;
+        model = mod;
+        size = siz;
+        color = colo;
+        wireless = wireles;
+        internet = interne;
+        camera = camer;
+    }
+
     void showPhone(){
         System.out.println(brand +", model " + model +", wielkość " +size +", kolor " +color+
                 ", czy jest berzprzewodowy ? " +wireless + ", czy posiada Internet ? " + internet
-        +", czy posiada aparat ? " + camera);
+                +", czy posiada aparat ? " + camera);
     }
-
-
 }
--- src/KlasyiObiekty/cellPhoneMain.java
+++ src/KlasyiObiekty/cellPhoneMain.java
@@ -2,23 +2,13 @@ package KlasyiObiekty;
 
 public class cellPhoneMain {
     public static void main(String[] args) {
-        Phone phone1 = new Phone();
-        phone1.brand = "Xiaomi";
-        phone1.model = "Redmi note 6 pro";
-        phone1.size = "mały";
-        phone1.color = "czarny";
-        phone1.wireless = true;
-        phone1.internet = true;
-        phone1.camera = true;
+        Phone phone1 = new Phone("Xiaomi","Redmi note 6 pro","mały","czarny"
+        ,true,true,true);
 
-        Phone phone2 = new Phone();
-        phone2.brand ="Sony";
-        phone2.model = "Xperia Z2";
-        phone2.size = "mały";
-        phone2.color = "biały";
-        phone2.wireless = true;
-        phone2.internet = true;
-        phone1.camera = false;
+        Phone phone2 = new Phone("Sony","Xperia Z2","mały","biały"
+        ,true,true,false);
+
+
 
         phone1.showPhone();
         phone2.showPhone();
